a29e29d9412ce16694d2c4ebff317266d6eef350
Piotr Wisniewski <piowisni@gmail.com>
2023-02-06 09:53:30 +0100

    Ex3, Ex4 Finished

--- Ex.cs
+++ Ex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,7 +35,6 @@ namespace Intersys
             {
                 Console.Write($"\n{i + 1}");
             }
-
             Console.ReadKey();
         }
 
@@ -43,13 +43,12 @@ namespace Intersys
             Console.WriteLine("Please provide a text");
             var userInput = Console.ReadLine().ToLower();
 
-
             List<char> inputChars = new List<char>();
             inputChars.AddRange(userInput);
             var finalstring = string
                 .Concat(inputChars
                 .Where(char.IsLetter))
-                .ToCharArray();  
+                .ToCharArray();
 
             Console.WriteLine(finalstring);
             for (int i = finalstring.Length - 1; i >= 0; --i)
@@ -59,9 +58,83 @@ namespace Intersys
 
             bool isPalindrome = finalstring.SequenceEqual(finalstring.Reverse());
 
-            Console.WriteLine($"\n{isPalindrome}");
+            string final = isPalindrome ? "YES" : "NO";
+            Console.WriteLine($"\n{final}");
+        }
+        public static void Ex3()
+        {
+
+            Console.WriteLine("Please provide numbers for the first array");
+
+            int[] userArray1 = new int[11];
+
+            for (int i = 0; i < 11; i++)
+            {
+                var input = int.TryParse(Console.ReadLine(), out int numberInput);
+                userArray1[i] = numberInput;
+            }
+
+            Console.WriteLine("Please provide numbers for the second array");
+            int[] userArray2 = new int[11];
+
+            for (int i = 0; i < 11; i++)
+            {
+                var input = int.TryParse(Console.ReadLine(), out int numberInput);
+                userArray2[i] = numberInput;
+            }
+            bool isPermutation = userArray2
+                .OrderByDescending(x => x)
+                .SequenceEqual(userArray1.OrderByDescending(x => x));
+            string final = isPermutation ? "YES" : "NO";
+            Console.WriteLine($"\n{final}");
+        }
+
+        public static void Ex4() //The excersize didnt specify if we want the numbers in the list to be unique or not
+        {
+            Console.WriteLine("Please provide numbers, simply provide 0 to finish the list");
+            List<int> userList = new List<int>();
+
+            do
+            {
+                var input = int.TryParse(Console.ReadLine(), out int userNumber);
+                bool isPowerOfTwo = BitOperations.IsPow2(userNumber);
+                bool isSqrtOfTwo = Math.Sqrt(userNumber) % 2 == 0;
+
+                if (isPowerOfTwo)
+                {
+                    userList.Add(userNumber);
+                    Console.WriteLine($"{userNumber} is a power of two, added to the list");
+                    if (isSqrtOfTwo)
+                    {
+                        userList.Add((int)Math.Sqrt(userNumber));
+                        Console.WriteLine($"Sqrt of {userNumber} == {(int)Math.Sqrt(userNumber)}, added to the list");
+                    }
+                }
+                if (userNumber == 0)
+                {
+                    break;
+                }
+
+            } while (true);
+
+
+            if (userList.Count == 0)
+            {
+                Console.WriteLine("NA");
+            }
+            else
+            {
+                Console.WriteLine("The Final list");
+                foreach (var number in userList)
+                {
+                    Console.Write($"{number},");
+                }
+            }
+        }
+
+        public void Ex5()
+        {
 
         }
-  
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Concurrent;
 
 
 //Ex.Ex1();
-Ex.Ex2();
-
+//Ex.Ex2();
+//Ex.Ex3();
+//Ex.Ex4();
+Ex.Ex5();
 
 
